f06934aadcf6e05fe48ff20b4ec1f7525271c2cd
Posiadacz07 <ulak.k@wp.pl>
2019-06-30 21:23:01 +0200

    Implementation of BinaryTree * Binary tree class * node class * basic insertion * find lowest common ancestor method * find node method

--- src/interface/BinaryTree.hpp
+++ src/interface/BinaryTree.hpp
@@ -1,29 +1,85 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
-#include <memory>
+#ifndef POSIADACZ07_ASSIGNMENT2_BINARY_TREE
+#define POSIADACZ07_ASSIGNMENT2_BINARY_TREE
 
-namespace trees {
+#include <vector>
 
-class Node {
-public:
-  Node(int newNodeValue);
-  int nodeValue;
-  std::unique_ptr<Node> leftNode;
-  std::unique_ptr<Node> rightNode;
-};
-
+//! Binary tree class
 class BinaryTree {
 public:
-  BinaryTree(int value);
+  BinaryTree() : rootNode(nullptr) {}
   ~BinaryTree();
 
-  void insertNode(int nodeValue);
-  bool searchNode(int nodeValue);
+  //! Inner class for node in binary tree with int key value
+  class Node {
+  public:
+    //! Getter of node key.
+    //! returns integer key of current node
+    int getKey() const;
+
+    //! Getter of left child of current node.
+    //! returns pointer to node which is left child of current node
+    Node *getLeftChild() const;
+
+    //! Getter of right child of current node.
+    //! returns pointer to node which is right child of current node
+    Node *getRightChild() const;
+
+  private:
+    Node(int nodeKey) : key(nodeKey), leftChild(nullptr), rightChild(nullptr) {}
+
+    //! Setter of left child of current node.
+    //! param[in] newLeftChild pointer to Node that's left child of current node
+    //! NOTE: If current node has left child it will be deleted with all their
+    //!       children.
+    void setLeftChild(Node *newLeftChild);
+
+    //! Setter of right child of current node.
+    //! param[in] newRightChild pointer to Node that's right child of current
+    //!           node
+    //! NOTE: If current node has right child it will be deleted with all their
+    //!       children.
+    void setRightChild(Node *newRightChild);
+
+    int key;
+    Node *leftChild;
+    Node *rightChild;
+
+    friend class BinaryTree;
+  };
+
+  //! Getter of root node in BinaryTree
+  //! returns pointer to rootNode
+  Node *getRootNode() const;
+
+  //! Setter of root node in BinaryTree
+  //! param[in] key key of root node to set
+  //! returns   pointer to already set node
+  //! NOTE: If root node exists it will be deleted with all their children.
+  Node *setRootNode(int key);
+  Node *insertLeftChild(Node *currentNode, int key);
+  Node *insertRightChild(Node *currentNode, int key);
+
+  //! Search for node with given key in current binary tree.
+  //! param[in] key value of key for searching node
+  //! returns   pointer to Node with given key if node exists, nullptr
+  //!           otherwise
+  Node *findNode(int key);
+
+  //! Search for ancestors of given node key in curren binary tree.
+  //! param[in] key of the node to find ancestors
+  //! returns   collection of keys for all the ancestors of the node with given
+  //!           key
+  std::vector<int> findAncestors(int key);
+
+  //! Search for lowest common ancestor in curren binary tree.
+  //! param[in] firstNode reference to first node
+  //! param[in] secondNode reference to second node
+  //! returns   pointer to node that is lowest common ancestor given two nodes
+  //!           if exists, nullptr otherwise
+  Node *lowestCommonAncestor(Node *firstNode, Node *secondNode);
 
 private:
-  std::unique_ptr<Node> rootNode;
+  Node *rootNode;
 };
 
-} // namespace trees
-
-#endif // BINARY_TREE_H
+#endif // POSIADACZ07_ASSIGNMENT2_BINARY_TREE
--- test/BinaryTreeTest.cpp
+++ test/BinaryTreeTest.cpp
@@ -1,5 +1,6 @@
-#include "../src/interface/BinaryTree.hpp"
+#include "src/interface/BinaryTree.hpp"
 #include "gtest/gtest.h"
+
 #include <vector>
 
 namespace trees {
@@ -15,8 +16,100 @@ void BinaryTreeTest::SetUp() {}
 
 void BinaryTreeTest::TearDown() {}
 
-TEST_F(BinaryTreeTest, SanityCheck) {
-  EXPECT_TRUE(true);
+// Helper function to set up tree for tests.
+//
+//            7
+//          /   \ 
+//         3     4
+//        / \     \  
+//       2   5     8
+//      / \ 
+//     1   6
+//
+BinaryTree prepareBinaryTree() {
+  BinaryTree tree;
+
+  BinaryTree::Node *root = tree.setRootNode(7);
+  tree.insertLeftChild(root, 3);
+  tree.insertRightChild(root, 4);
+  tree.insertLeftChild(root->getLeftChild(), 2);
+  tree.insertRightChild(root->getLeftChild(), 5);
+  tree.insertRightChild(root->getRightChild(), 8);
+  tree.insertLeftChild(root->getLeftChild()->getLeftChild(), 1);
+  tree.insertRightChild(root->getLeftChild()->getLeftChild(), 6);
+
+  return tree;
+}
+
+TEST_F(BinaryTreeTest, FindNodeInTree) {
+  BinaryTree tree = prepareBinaryTree();
+  BinaryTree::Node *findedNode = tree.findNode(5);
+  EXPECT_NOT_NULL(findedNode);
+  EXPECT_EQ(5, findedNode->getKey());
+  EXPECT_NULL(findedNode->getLeftChild());
+  EXPECT_NULL(findedNode->getRightChild());
+}
+
+TEST_F(BinaryTreeTest, FindNodeWithKeyNotExistingInTree) {
+  BinaryTree tree = prepareBinaryTree();
+  BinaryTree::Node *findedNode = tree.findNode(100);
+  EXPECT_NULL(findedNode);
+}
+
+TEST_F(BinaryTreeTest, FindNodeInEmptyTree) {
+  BinaryTree tree;
+  BinaryTree::Node *findedNode = tree.findNode(100);
+  EXPECT_NULL(findedNode);
+}
+
+TEST_F(BinaryTreeTest, FindAncestorsInTree) {
+  BinaryTree tree = prepareBinaryTree();
+  std::vector<int> ancestors = tree.findAncestors(8);
+  std::vector<int> expectedAncestors{4, 7};
+  EXPECT_EQ(expectedAncestors, ancestors);
+}
+
+TEST_F(BinaryTreeTest, FindAncestorsOfRoot) {
+  BinaryTree tree = prepareBinaryTree();
+  std::vector<int> ancestors = tree.findAncestors(7);
+  std::vector<int> expectedAncestors;
+  EXPECT_EQ(expectedAncestors, ancestors);
+}
+
+TEST_F(BinaryTreeTest, FindAncestorsInEmptyTree) {
+  BinaryTree tree;
+  std::vector<int> ancestors = tree.findAncestors(8);
+  std::vector<int> expectedAncestors;
+  EXPECT_EQ(expectedAncestors, ancestors);
+}
+
+TEST_F(BinaryTreeTest, FindLowestCommonAncestorInTree) {
+  BinaryTree tree = prepareBinaryTree();
+  BinaryTree::Node *firstNode = tree.findNode(5);
+  BinaryTree::Node *secondNode = tree.findNode(6);
+  BinaryTree::Node *lowestCommonAncestor =
+      tree.lowestCommonAncestor(firstNode, secondNode);
+  EXPECT_EQ(3, lowestCommonAncestor->getKey());
+}
+
+TEST_F(BinaryTreeTest, FindLowestCommonAncestorInTreeWithRoot) {
+  BinaryTree tree = prepareBinaryTree();
+  BinaryTree::Node *firstNode = tree.findNode(7);
+  BinaryTree::Node *secondNode = tree.findNode(6);
+  BinaryTree::Node *lowestCommonAncestor =
+      tree.lowestCommonAncestor(firstNode, secondNode);
+  EXPECT_EQ(7, lowestCommonAncestor->getKey());
+}
+
+TEST_F(BinaryTreeTest, FindLowestCommonAncestorInTreeWithNotExistingNode) {
+  BinaryTree tree = prepareBinaryTree();
+  BinaryTree::Node *firstNode = tree.findNode(100);
+  EXPECT_NULL(firstNode);
+  BinaryTree::Node *secondNode = tree.findNode(6);
+  EXPECT_NOT_NULL(secondNode);
+  BinaryTree::Node *lowestCommonAncestor =
+      tree.lowestCommonAncestor(firstNode, secondNode);
+  EXPECT_NULL(lowestCommonAncestor);
 }
 } // namespace test
 } // namespace trees
