a7c4a31c574b6fc0589f80e8876e28afcf38e4e5
Urszula Krzeszewska <ulak.k@wp.pl>
2019-07-26 13:30:28 +0200

    Morris-Pratt algorith to pattern search in string implementation

--- src/implementation/strings/strings.cpp
+++ src/implementation/strings/strings.cpp
@@ -3,29 +3,108 @@
 #include <string>
 #include <vector>
 
-namespace strings {
+namespace strings
+{
+
+//! Helper function to check validity of source string and pattern string.
+//! Some how sanity chaeck.
+//! param[in] source  string in which pattern is searched
+//! param[in] pattern string to search in source
+//! returns
+bool checkValidityOfSourceAndPattern(std::string source, std::string pattern)
+{
+  return source != "" && pattern != "" && pattern.length() <= source.length();
+}
 
 std::vector<int> searchPatternBruteForce(std::string source,
-                                         std::string pattern) {
+                                         std::string pattern)
+{
   std::vector<int> result;
-  if (source == "" || pattern == "" || pattern.length() > source.length()) {
+  if (!checkValidityOfSourceAndPattern(source, pattern))
+  {
     return result;
   }
 
-  for (int i = 0; i < source.length() - pattern.length() + 1; i++) {
+  for (int i = 0; i < source.length() - pattern.length() + 1; i++)
+  {
     bool patternFound = true;
-    for (int j = 0; j < pattern.length(); j++) {
-      if (source[i + j] != pattern[j]) {
+    for (int j = 0; j < pattern.length(); j++)
+    {
+      if (source[i + j] != pattern[j])
+      {
         patternFound = false;
         break;
       }
     }
 
-    if (patternFound) {
+    if (patternFound)
+    {
       result.push_back(i);
     }
   }
   return result;
 }
 
+//! Helper function to generate max prefix borders of pattern.
+//! param[in] pattern string with pattern to search
+//! returns   collection of max prefix borders of pattern
+std::vector<int> generateMaxPrefixBorders(std::string pattern)
+{
+  if (pattern == "")
+  {
+    return {};
+  }
+  std::vector<int> result;
+  result.reserve(pattern.length() + 1);
+  result.at(0) = -1;
+  int border = -1;
+
+  for (int i = 0; i < pattern.length(); i++)
+  {
+    while (border > -1 && pattern[border] != pattern[i])
+    {
+      border = result.at(border);
+    }
+    border++;
+    result.at(i) = border;
+  }
+  return result;
+}
+
+std::vector<int> searchPatternMorrisPratt(std::string source,
+                                          std::string pattern)
+{
+  std::vector<int> result;
+  if (!checkValidityOfSourceAndPattern(source, pattern))
+  {
+    return result;
+  }
+
+  std::vector<int> maxPrefixBorders = generateMaxPrefixBorders(pattern);
+  int sourceIndex = 0;
+  int patternIndex = 0;
+
+  while (sourceIndex < source.length())
+  {
+    if (pattern[patternIndex] == source[sourceIndex])
+    {
+      patternIndex++;
+      sourceIndex++;
+      if (patternIndex == pattern.length())
+      {
+        result.push_back(sourceIndex - patternIndex);
+      }
+    }
+    else
+    {
+      patternIndex = maxPrefixBorders[patternIndex];
+      if (patternIndex < 0)
+      {
+        sourceIndex++;
+        patternIndex++;
+      }
+    }
+  }
+  return result;
+}
 } // namespace strings
--- src/interface/strings.hpp
+++ src/interface/strings.hpp
@@ -4,11 +4,19 @@
 #include <string>
 #include <vector>
 
-namespace strings {
+namespace strings
+{
 
+//! Function to search pattern in string using brute force.
+//! param[in] source  string in which pattern is searched
+//! param[in] pattern string to search in source
+//! returns   collection of indexes of every pattern start in source
 std::vector<int> searchPatternBruteForce(std::string source,
                                          std::string pattern);
 
+//! Function to search pattern in string using Morris-Pratt algorithm.
+std::vector<int> searchPatternMorrisPratt(std::string source,
+                                          std::string pattern);
 } // namespace strings
 
 #endif // STRINGS_HPP
