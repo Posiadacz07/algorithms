0f4919c35241b3e94ca84e10a70cb8da5a5095ca
Urszula Krzeszewska <ulak.k@wp.pl>
2019-10-11 12:52:01 +0200

    Karp-Rabin search pattern algorithm implemented and documented

--- src/implementation/strings/strings.cpp
+++ src/implementation/strings/strings.cpp
@@ -3,34 +3,55 @@
 #include <string>
 #include <vector>
 
-namespace strings {
+namespace strings
+{
 
 //! Helper function to check validity of source string and pattern string.
 //! Some how sanity chaeck.
 //! param[in] source  string in which pattern is searched
 //! param[in] pattern string to search in source
-//! returns   true if source and pattern are valid, false otherwise
-bool checkValidityOfSourceAndPattern(std::string source, std::string pattern) {
+//! returns   true if source and pattern are valid, false otherwise.
+bool checkValidityOfSourceAndPattern(std::string source, std::string pattern)
+{
   return source != "" && pattern != "" && pattern.length() <= source.length();
 }
 
+//! Helper function to check if pattern is found in the source at given index.
+//! param[in] source               string in which pattern is searched
+//! param[in] pattern              string to search in source
+//! param[in] currentIndexOfSource index in source string where pattern search starts
+//! retruns   true if pattern is found at given index, false otherwise.
+bool checkPatternFound(const std::string &source,
+                       const std::string &pattern,
+                       int currentIndexOfSource)
+{
+  bool patternFound = true;
+  for (int j = 0; j < pattern.length(); j++)
+  {
+    if (source[currentIndexOfSource + j] != pattern[j])
+    {
+      patternFound = false;
+      break;
+    }
+  }
+  return patternFound;
+}
+
 std::vector<int> searchPatternBruteForce(std::string source,
-                                         std::string pattern) {
+                                         std::string pattern)
+{
   std::vector<int> result;
-  if (!checkValidityOfSourceAndPattern(source, pattern)) {
+  if (!checkValidityOfSourceAndPattern(source, pattern))
+  {
     return result;
   }
 
-  for (int i = 0; i < source.length() - pattern.length() + 1; i++) {
-    bool patternFound = true;
-    for (int j = 0; j < pattern.length(); j++) {
-      if (source[i + j] != pattern[j]) {
-        patternFound = false;
-        break;
-      }
-    }
+  for (int i = 0; i < source.length() - pattern.length() + 1; i++)
+  {
+    bool patternFound = checkPatternFound(source, pattern, i);
 
-    if (patternFound) {
+    if (patternFound)
+    {
       result.push_back(i);
     }
   }
@@ -40,8 +61,10 @@ std::vector<int> searchPatternBruteForce(std::string source,
 //! Helper function to generate max prefix borders of pattern.
 //! param[in] pattern string with pattern to search
 //! returns   collection of max prefix borders of pattern
-std::vector<int> generateMaxPrefixBorders(std::string pattern) {
-  if (pattern == "") {
+std::vector<int> generateMaxPrefixBorders(std::string pattern)
+{
+  if (pattern == "")
+  {
     return {};
   }
   std::vector<int> result;
@@ -49,8 +72,10 @@ std::vector<int> generateMaxPrefixBorders(std::string pattern) {
   result.at(0) = -1;
   int border = -1;
 
-  for (int i = 0; i < pattern.length(); i++) {
-    while (border > -1 && pattern[border] != pattern[i]) {
+  for (int i = 0; i < pattern.length(); i++)
+  {
+    while (border > -1 && pattern[border] != pattern[i])
+    {
       border = result.at(border);
     }
     border++;
@@ -60,9 +85,11 @@ std::vector<int> generateMaxPrefixBorders(std::string pattern) {
 }
 
 std::vector<int> searchPatternMorrisPratt(std::string source,
-                                          std::string pattern) {
+                                          std::string pattern)
+{
   std::vector<int> result;
-  if (!checkValidityOfSourceAndPattern(source, pattern)) {
+  if (!checkValidityOfSourceAndPattern(source, pattern))
+  {
     return result;
   }
 
@@ -70,16 +97,22 @@ std::vector<int> searchPatternMorrisPratt(std::string source,
   int sourceIndex = 0;
   int patternIndex = 0;
 
-  while (sourceIndex < source.length()) {
-    if (pattern[patternIndex] == source[sourceIndex]) {
+  while (sourceIndex < source.length())
+  {
+    if (pattern[patternIndex] == source[sourceIndex])
+    {
       patternIndex++;
       sourceIndex++;
-      if (patternIndex == pattern.length()) {
+      if (patternIndex == pattern.length())
+      {
         result.push_back(sourceIndex - patternIndex);
       }
-    } else {
+    }
+    else
+    {
       patternIndex = maxPrefixBorders[patternIndex];
-      if (patternIndex < 0) {
+      if (patternIndex < 0)
+      {
         sourceIndex++;
         patternIndex++;
       }
@@ -88,59 +121,50 @@ std::vector<int> searchPatternMorrisPratt(std::string source,
   return result;
 }
 
-//! Helper fuction to check first index in Karp-Rabin algorithm with hashes.
-bool checkKarpRabinFirstIndex(std::string source, std::string pattern,
-                              int primeValue, int &sigSource, int &sigPattern) {
-  sigSource = 0;
-  sigPattern = 0;
-  for (int i = 0; i < pattern.length(); i++) {
-    sigSource = (sigSource * 256 + source.at(i)) % primeValue;
-    sigPattern = (sigPattern * 256 + source.at(i)) % primeValue;
+//! Helper function to calculate hash for Karp-Rabin algorithm.
+//! Hash is the sum of the all letters in ASCII.
+long long calculateHash(std::string word)
+{
+  long long result = 0;
+  for (char letter : word)
+  {
+    result += letter;
   }
-
-  if (sigSource == sigPattern) {
-    return true;
-  }
-  return false;
-}
-
-//! Helper function to calculate power in Karp-Rabin algorithm used in next
-//! steps.
-int calculateKarpRabinPower(int patternLength, int primeValue) {
-  int power = 1;
-  for (int i = 1; i < patternLength; i++) {
-    power = (power * 256) % primeValue;
-  }
-  return power;
+  return result;
 }
 
 std::vector<int> searchPatternKarpRabin(std::string source,
-                                        std::string pattern) {
+                                        std::string pattern)
+{
   std::vector<int> result;
-  if (!checkValidityOfSourceAndPattern(source, pattern)) {
+  if (!checkValidityOfSourceAndPattern(source, pattern))
+  {
     return result;
   }
-  int primeValue = 100007;
-  int sigSource, sigPattern;
-  if (checkKarpRabinFirstIndex(source, pattern, primeValue, sigSource,
-                               sigPattern)) {
-    result.push_back(0);
-  }
 
-  int power = calculateKarpRabinPower(pattern.length(), primeValue);
+  long long patternHash = calculateHash(pattern);
+  long long sourceHash = calculateHash(source.substr(0, pattern.length()));
 
-  for (int i = 0; i < source.length() - pattern.length(); i++) {
-    sigSource = (sigSource + primeValue - power * source.at(i) % primeValue) %
-                primeValue;
-    sigSource =
-        (sigSource * 256 + source.at(i + pattern.length())) % primeValue;
+  int i = 0;
+  while (i + pattern.length() - 1 < source.length())
+  {
+    if (patternHash == sourceHash)
+    {
+      bool patternFound = checkPatternFound(source, pattern, i);
 
-    if (sigSource == sigPattern &&
-        source.substr(i, pattern.length()) == pattern) {
-      result.push_back(i);
+      if (patternFound)
+      {
+        result.push_back(i);
+      }
+    }
+
+    i++;
+
+    if (i + pattern.length() - 1 < source.length())
+    {
+      sourceHash = calculateHash(source.substr(i, pattern.length()));
     }
   }
-
   return result;
 }
 
